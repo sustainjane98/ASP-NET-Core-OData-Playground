1ab0bab3cd312a4e4dfd7f3cd6d3b02c22747525
Jane Will <jwill@brockhaus-ag.de>
2023-07-23 18:26:47 +0200

    Update Router

--- OdataPlayground/OdataPlaygroundApplication/apps/odata-playground/src/configs/routing-config.tsx
+++ OdataPlayground/OdataPlaygroundApplication/apps/odata-playground/src/configs/routing-config.tsx
@@ -1,6 +1,7 @@
-import { RouteObject } from "react-router-dom";
-import IndexPage from "../pages";
+import { RouteObject, Navigate } from 'react-router-dom';
+import IndexPage from '../pages';
 
 export const routingConfig = [
-  { path: "/", element: <IndexPage /> },
+  { path: '/', element: <IndexPage /> },
+  { path: '*', element: <Navigate to={'/'} /> },
 ] as RouteObject[];
