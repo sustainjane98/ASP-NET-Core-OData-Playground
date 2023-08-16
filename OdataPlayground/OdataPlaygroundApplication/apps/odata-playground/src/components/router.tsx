2860165823c650e8baddfa03f0ccd68d2164ba87
Jannik Will <jw19980309@gmail.com>
2023-08-16 07:40:26 +0200

    fix: Fix issue in dev mode
    Fix issue in dev mode

--- OdataPlayground/OdataPlaygroundApplication/apps/odata-playground/src/components/router.tsx
+++ OdataPlayground/OdataPlaygroundApplication/apps/odata-playground/src/components/router.tsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routingConfig } from '../configs/routing-config';
 
-const br = createBrowserRouter(routingConfig, { basename: '/odata' });
+// eslint-disable-next-line no-restricted-globals
+const path = location?.pathname;
+
+const br = createBrowserRouter(routingConfig, { basename: path });
 
 export const Router = () => {
   return <RouterProvider router={br} />;
