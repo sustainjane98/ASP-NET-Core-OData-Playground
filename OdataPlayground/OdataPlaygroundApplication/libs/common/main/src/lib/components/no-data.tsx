fd740ee5921fda8c5ae1bf976cb5955936c5fa44
Jannik Will <jw19980309@gmail.com>
2023-09-20 07:17:37 +0200

    Updated svg conversion

--- OdataPlayground/OdataPlaygroundApplication/libs/common/main/src/lib/components/no-data.tsx
+++ OdataPlayground/OdataPlaygroundApplication/libs/common/main/src/lib/components/no-data.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { ReactComponent as NoDataIllustration } from '../assets/icons/vectors/undraw_no_data_illustration.svg';
+import { UndrawNoDataIllustration } from '../assets/icons/vectors/generated';
 
 export const NoData = () => {
   return (
     <div className="flex-1 flex justify-center items-center w-full min- h-full">
       <div className="flex flex-col items-center">
-        <NoDataIllustration width={200} height={200} />
+        <UndrawNoDataIllustration width={200} height={200} />
         <span className="text-base font-medium ml-12 mt-6">
           No Data available
         </span>
